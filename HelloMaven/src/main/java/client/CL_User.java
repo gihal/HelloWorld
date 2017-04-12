1dac8b500173538d5aa29c91cfd00d74a611502e
gihal <gihal83@gmail.com>
2017-04-12 15:33:15 +1200

    added new client object

---
+++ HelloMaven/src/main/java/client/CL_User.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package client;
+
+/**
+ *
+ * @author gihal
+ */
+public class CL_User {
+
+    private int id;
+    private String userName;
+    private String state;
+
+    public CL_User(int id, String userName, String state) {
+        this.id = id;
+        this.userName = userName;
+        this.state = state;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+}
