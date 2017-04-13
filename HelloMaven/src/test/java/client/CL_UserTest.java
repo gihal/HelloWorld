1caaaf0b7a52c6b892b7fa5b363ccb07eb23a8ff
gihal <gihal83@gmail.com>
2017-04-14 01:13:23 +1200

    added new test class

---
+++ HelloMaven/src/test/java/client/CL_UserTest.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package client;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author gihal
+ */
+public class CL_UserTest {
+
+    public CL_UserTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getId method, of class CL_User.
+     */
+    @org.junit.Test
+    public void testGetId() {
+        System.out.println("getId");
+    }
+
+    /**
+     * Test of setId method, of class CL_User.
+     */
+    @org.junit.Test
+    public void testSetId() {
+        System.out.println("setId");
+    }
+
+    /**
+     * Test of getUserName method, of class CL_User.
+     */
+    @org.junit.Test
+    public void testGetUserName() {
+        System.out.println("getUserName");
+    }
+
+    /**
+     * Test of setUserName method, of class CL_User.
+     */
+    @org.junit.Test
+    public void testSetUserName() {
+        System.out.println("setUserName");
+    }
+
+    /**
+     * Test of getState method, of class CL_User.
+     */
+    @org.junit.Test
+    public void testGetState() {
+        System.out.println("getState");
+    }
+
+    /**
+     * Test of setState method, of class CL_User.
+     */
+    @org.junit.Test
+    public void testSetState() {
+        System.out.println("setState");
+    }
+
+    /**
+     * Test of toString method, of class CL_User.
+     */
+    @org.junit.Test
+    public void testToString() {
+        System.out.println("toString");
+    }
+
+}
